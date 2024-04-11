e054fd20367c39a1c2a8c1eb52d11afc52260b50
Santi Singha <santisingha191@gmail.com>
2024-04-11 21:18:10 +0530

    final commit

---
+++ app/Home.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { useDraw } from "@/hooks/useDraw";
+import { useState } from "react";
+import { ChromePicker } from "react-color";
+
+export default function Home() {
+  const [color, setColor] = useState<string>("#000");
+
+  const { canvasRef, onMouseDown, clear } = useDraw(drawLine);
+
+  function drawLine({ prevPoint, currentPoint, ctx }: Draw) {
+    const { x: currX, y: currY } = currentPoint;
+    const lineColor = color;
+    const lineWidth = 5;
+
+    let startPoint = prevPoint ?? currentPoint;
+    ctx.beginPath();
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = lineColor;
+    ctx.moveTo(startPoint.x, startPoint.y);
+    ctx.lineTo(currX, currY);
+    ctx.stroke();
+
+    ctx.fillStyle = lineColor;
+    ctx.beginPath();
+    ctx.arc(startPoint.x, startPoint.y, 2, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
+  return (
+    <div className="w-screen h-screen flex justify-center items-center bg-white ">
+      <div className="flex flex-col gap-10 pr-10">
+        <ChromePicker color={color} onChange={(e) => setColor(e.hex)} />
+        <button onClick={clear} className="p-2 rounded-lg border border-black">
+          Clear Canvas
+        </button>
+      </div>
+      <canvas
+        onMouseDown={onMouseDown}
+        ref={canvasRef}
+        width={750}
+        height={750}
+        className="border border-black rounded-md"
+      />
+    </div>
+  );
+}
